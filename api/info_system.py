e8d9179d01e9b1db49d4a0a22c6a2dcfeca43ef3
Harry_Tong <2754923267@qq.com>
2024-11-21 09:33:21 +0800

    init

--- api/info_system.py
+++ api/info_system.py
@@ -8,7 +8,7 @@ QUERY_TYPE = "site"
 def fetch_info_system_data(query):
     # 设置请求参数
     params = INFO_SYSTEM_PARAMS.copy()  # 复制默认参数
-    params["query"] = f'company={query}&&(status_code=200||status_code="")&&url=!""'
+    params["query"] = f'title=="{query}"||company={query}&&(status_code=200||status_code="")&&url=!""'
     # params["query"] = f'company={query}&&status_code=200&&url=!""'
     params["query_type"] = QUERY_TYPE
 
